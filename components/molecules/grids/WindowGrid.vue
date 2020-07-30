9cf9b73ea8d36c5c4217f2d29094091dfa6083ba
OkaKyo <0622okakyo@gmail.com>
2020-07-30 16:54:17 +0900

    コンポーネントを再整理

--- components/molecules/grids/WindowGrid.vue
+++ components/molecules/grids/WindowGrid.vue
@@ -1,5 +1,9 @@
 <template>
-  <div />
+  <v-row justify="center">
+    <v-col cols="12" md="8">
+      <slot />
+    </v-col>
+  </v-row>
 </template>
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
--- components/organisms/lists/setting/SettingListContent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div />
-</template>
-<script lang="ts">
-import { defineComponent } from '@vue/composition-api'
-export default defineComponent({
-  name: 'SEttingListContent',
-})
-</script>
