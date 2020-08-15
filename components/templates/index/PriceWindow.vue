841223876f428ed7d4e23aa044742d728b6299ce
OkaKyo <0622okakyo@gmail.com>
2020-08-15 21:34:46 +0900

    WIP: サンプルページを修正する必要あり

--- components/templates/index/PriceWindow.vue
+++ components/templates/index/PriceWindow.vue
@@ -1,30 +1,31 @@
 <template>
   <v-container>
-    <v-lazy
-      v-model="isOn"
-      :options="{
-        threshold: 0.5,
-      }"
-      transition="fade-transition"
-    >
-      <v-row justify="center">
-        <v-col v-for="i in 3" :key="i" cols="8" md="4" xl="3">
+    <v-row justify="center">
+      <v-col v-for="i in 3" :key="i" cols="8" md="4" xl="3">
+        <v-lazy
+          v-model="lazyDescript[index]"
+          :options="{
+            threshold: 1.0,
+          }"
+          transition="fade-transition"
+        >
           <v-card min-height="400px">
             <v-card-title></v-card-title>
           </v-card>
-        </v-col>
-      </v-row>
-    </v-lazy>
+        </v-lazy>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from '@vue/composition-api'
+import { defineComponent, reactive } from '@vue/composition-api'
 export default defineComponent({
   name: 'PriceWindow',
   setup() {
-    const isOn = ref(false)
+    const lazyDescript = reactive<boolean[]>([false, false, false])
+
     return {
-      isOn,
+      lazyDescript,
     }
   },
 })
--- content/about/fast-py.md
+++ content/about/fast-py.md
@@ -7,5 +7,6 @@ thumbnail: ""
 FastAPI を利用して Python のサーバーを立てます:smile:
 ## 使用するフレームワーク
 ## 仕様について
+- 次のページを参照：<nuxt-link to="specification/fast-py">仕様書ページ</nuxt-link>
 ## TODOs
 ## 今後のについて
