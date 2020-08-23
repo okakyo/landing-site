ce18ac25de33504e3be93cedd97e92cdc084edd1
OkaKyo <0622okakyo@gmail.com>
2020-08-24 05:09:57 +0900

    ログアウトしたらヘッダ部分も再描写されるように修正

--- pages/login.vue
+++ pages/login.vue
@@ -7,11 +7,13 @@ export default Vue.extend({
   name: 'Login',
   async fetch({ $axios, route, redirect, app }) {
     const code = route.query.code
+    console.log(code)
     const sendData = {
       client_id: process.env.GITHUB_CLIENT_ID,
       client_secret: process.env.GITHUB_CLIENT_SECRET,
       code,
     }
+    console.log(sendData)
     try {
       const res = await $axios.post(
         'https://github.com/login/oauth/access_token',
