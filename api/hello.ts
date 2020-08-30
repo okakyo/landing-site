50fe5aacb77f228b59055210d3d9d3998c48be11
OkaKyo <0622okakyo@gmail.com>
2020-08-30 12:26:43 +0900

    動作確認

--- api/hello.ts
+++ api/hello.ts
@@ -3,5 +3,5 @@ import axios from 'axios'
 // ここで認証周りを実装できるなら実装したい
 export default async (request: NowRequest, response: NowResponse) => {
   const res = await axios.get('https://github.com/okakyo/landing-site')
-  return response.send(res.data)
+  return response.send(res.data.title)
 }
