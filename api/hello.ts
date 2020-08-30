00e28f8ba302df2b7d50ba564cd94a928ecf1557
OkaKyo <0622okakyo@gmail.com>
2020-08-30 13:18:32 +0900

    動作確認

--- api/hello.ts
+++ api/hello.ts
@@ -1,10 +1,13 @@
 import { NowRequest, NowResponse } from '@vercel/node'
+import { JSDOM } from 'jsdom'
 import axios from 'axios'
 // ここで認証周りを実装できるなら実装したい
 export default async (request: NowRequest, response: NowResponse) => {
   const res = await axios.get('https://github.com/okakyo/landing-site')
-
+  const html = new JSDOM(res.data).window.document
+  const title = html.title
   return response.json({
-    res,
+    title,
+    meta: html.getElementsByTagName('meta'),
   })
 }
