cfb4526b3b9803fca31ef009ee21a839afdae5ef
OkaKyo <0622okakyo@gmail.com>
2020-09-01 23:29:27 +0900

    検索システムを構築

--- components/molecules/grids/BaseGrid.vue
+++ components/molecules/grids/BaseGrid.vue
@@ -8,5 +8,7 @@
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
-export default defineComponent({})
+export default defineComponent({
+  name: 'BaseGrid',
+})
 </script>
--- components/organisms/cards/ProjectCard.vue
+++ components/organisms/cards/ProjectCard.vue
@@ -1,9 +1,35 @@
 <template>
-  <v-card></v-card>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="(card, i) in docLists"
+        :key="i"
+        cols="6"
+        md="4"
+        lg="3"
+        xl="2"
+      >
+        <v-card :to="'/docs' + card.dir + '/' + card.slug">
+          <v-img
+            :src="
+              card.thumbnail === '' ? '/images/404-min.png' : card.thumbnail
+            "
+          />
+          <v-card-subtitle>{{ card.title }}</v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 export default defineComponent({
   name: 'ProjectCard',
+  props: {
+    docLists: {
+      type: Object,
+      required: true,
+    },
+  },
 })
 </script>
