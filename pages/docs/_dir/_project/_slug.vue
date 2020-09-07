79630ea7ccb6545db2c099e3d8943bfc630eafd5
OkaKyo <0622okakyo@gmail.com>
2020-09-07 22:36:07 +0900

    修正

--- pages/docs/_dir/_project/_slug.vue
+++ pages/docs/_dir/_project/_slug.vue
@@ -5,21 +5,10 @@
 import { defineComponent } from '@vue/composition-api'
 import ArticleWindow from '@/components/templates/specification/ArticleWindow.vue'
 
-import axios from 'axios'
-
-const apiMeta = axios.create({
-  withCredentials: process.env.NODE_ENV !== 'production',
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest',
-    'Access-Control-Allow-Origin': '*',
-  },
-  timeout: 20000,
-})
-
 export default defineComponent({
   name: 'question',
   layout: 'doc',
-  async asyncData({ $content, params, $axios }) {
+  async asyncData({ $content, params }) {
     const page = await $content(
       `/${params.dir}/${params.project}`,
       params.slug
@@ -27,8 +16,11 @@ export default defineComponent({
     const docLists = await $content('projects/about')
       .only(['title', 'thumbnail', 'slug', 'dir'])
       .fetch()
-    const getInfo = await apiMeta.get('https://github.com/okakyo/landing-site')
-    console.log(getInfo.headers)
+    fetch('https://github.com/okakyo/landing-site').then(async (res) => {
+      const data = await res.text()
+      console.log(data)
+    })
+
     return {
       page,
       docLists,
